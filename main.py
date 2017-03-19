fe9659d69de6c1747ffe3ab039b78442204bfe19
tunaonur <onur1tuna@gmail.com>
2017-03-19 04:14:38 -0700

    Posting Images on Twitter using Alexa

--- main.py
+++ main.py
@@ -5,8 +5,22 @@ import calendar, time, json
 from analysis import vision
 from pprint import pprint
 
+from tweepy import API
+from tweepy import OAuthHandler
+import requests
+import os
+import urlparse, urllib
+
 app = Flask(__name__)
 ask = Ask(app, '/torch')
+
+
+ckey = 'tC8oIP1dzUVB0j54V2RPmgkim'
+csecret = 'oH6os7iZBJKeJm4k8BED6z9vLRthc8ne82qUZTHgRKe6KAr9Vj'
+atoken = '843351930999001088-IvK8bbBP3yONpMBOJXMJIWJ1B99OEuF'
+asecret = '0gPmC4qKGJgMsM82wJGSirUOoVcppm9CyqoppdVssOI0g'
+
+
 
 @app.route('/')
 def homepage():
@@ -34,6 +48,26 @@ def take_picture():
 
 def send_picture(filepath):
     return vision(filepath)
+
+@ask.intent("Tweet")
+def capture_and_tweet():
+    filepath = take_picture()
+    data = send_picture(filepath)
+    view_description = get_description(data)
+    tweet_image(view_description, filepath)
+    print("Sick tweet")
+
+def tweet_image(message, filepath):
+    api = twitter_api()
+    media = api.media_upload(filepath)
+    api.update_status(status=message, media_ids=[media.media_id])
+
+def twitter_api():
+    auth = OAuthHandler(ckey, csecret)
+    auth.set_access_token(atoken, asecret)
+    api = API(auth)
+    return api
+
 
 def get_description(data):
     python_obj = json.loads(data)
